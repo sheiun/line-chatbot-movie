4660383402d416727b2dab86ae1174ebff57a7ef
sheiun <me.sheiun@gmail.com>
2018-12-28 00:38:25 +0800

    build: v1

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-import express from 'express'
+'use strict';
 
+const line = require('@line/bot-sdk');
+const express = require('express');
 const cfenv = require('cfenv')
 
-const app = express()
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
 
-app.use(express.static(__dirname + '/public'))
+const client = new line.Client(config);
+const app = express();
+
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
 
 const appEnv = cfenv.getAppEnv()
 
 app.listen(appEnv.port, '0.0.0.0', () => console.log("server starting on " + appEnv.url))
 
-// module.exports = require('./services/bot.js')
+// module.exports = require('./services/movie.js')
