b362502bf8b15c6b3dca7cd84e9e4efe9fdd7339
sheiun <me.sheiun@gmail.com>
2018-12-28 00:21:10 +0800

    test: es6 babel

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 
-var cfenv = require('cfenv')
+const cfenv = require('cfenv')
 
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-var appEnv = cfenv.getAppEnv()
+const appEnv = cfenv.getAppEnv()
 
 app.listen(appEnv.port, '0.0.0.0', () => console.log("server starting on " + appEnv.url))
+
+// module.exports = require('./services/bot.js')
