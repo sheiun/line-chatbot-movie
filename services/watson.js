dfe1af3c2e5d5cc04f9c7ce274dfcd2114d8e6b0
sheiun <me.sheiun@gmail.com>
2018-12-29 22:25:40 +0800

    feat: callAssistant

--- services/watson.js
+++ services/watson.js
@@ -1,17 +1,24 @@
-var AssistantV1 = require('watson-developer-cloud/assistant/v1');
+const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 
-var assistant = new AssistantV1({
+const assistant = new AssistantV1({
   version: '2018-09-20',
   iam_apikey: 'uWyAZfbl_GA85v_qmzTKVWTJ3GfCqPqlrLyNyqhjf3wP',
   url: 'https://gateway.watsonplatform.net/assistant/api'
 });
 
-assistant.message({
-  workspace_id: '38da265a-084d-40e8-8d87-6ff1cfe537f5',
-  input: { 'text': '我想' }
-}, (err, resp) => {
-  if (err)
-    console.log('error:', err);
-  else
-    console.log(JSON.stringify(resp, null, 2));
-});
+const callAssistant = (text, conversation_id = null) => {
+  return new Promise((resolve, reject) => {
+    assistant.message({
+      workspace_id: '38da265a-084d-40e8-8d87-6ff1cfe537f5', // Movie
+      input: { 'text': text },
+      context: { 'conversation_id': conversation_id }
+    }, (err, resp) => {
+      if (err)
+        reject(err)
+      else
+        resolve(resp);
+    });
+  })
+}
+
+module.exports = { callAssistant };
